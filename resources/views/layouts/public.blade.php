bc44357e7238fe72d1eeea9773e0217cb91f36f9
hyquoc09az <hyquoc09az@gmail.com>
2026-06-15 15:27:29 +0700

    Use CDN for bootstrap-icons in public layout to fix MIME issues

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -15,7 +15,8 @@
         <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300;500&display=swap" rel="stylesheet">
 
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/bootstrap-icons.css') }}" rel="stylesheet">
+        <!-- Sử dụng CDN cho bootstrap-icons để tránh lỗi MIME type chặn file font trên một số server (IIS) -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         <link href="{{ asset('css/templatemo-barber-shop.css') }}" rel="stylesheet">
         <style>
             /* ===== SIDEBAR MENU STYLES ===== */
